dd4f806a1bab75a64eefd2f352f30846e9d28b09
john Onyekachi <johnmikeonyekachi@gmail.com>
2021-12-31 17:14:20 +0000

    Node JS server

---
+++ backend/data.js
@@ -0,0 +1,89 @@
+const data ={
+    products:[
+        {
+            _id:'1',
+            name:'Name Slim Shirt',
+            category: 'Shirts',
+            image:'/images/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description:'high quality product',
+
+        },
+        {
+            _id:'2',
+            name:'Adidas Slim ',
+            category: 'Shirts',
+            image:'/images/p1.jpg',
+            price: 100,
+            brand: 'Nike',
+            rating: 4,
+            numReviews: 10,
+            description:'high quality product',
+
+        },
+        {
+            _id:'3',
+            name:'Pepsi Shirt',
+            category: 'Shirts',
+            image:'/images/p1.jpg',
+            price: 90,
+            brand: 'pepsi',
+            rating: 3.5,
+            numReviews: 14,
+            description:'high quality product',
+
+        },
+        {
+            _id:'4',
+            name:'Adidas Slim Shirt',
+            category: 'Shirts',
+            image:'/images/p1.jpg',
+            price: 10,
+            brand: 'Adidas',
+            rating: 3.5,
+            numReviews: 10,
+            description:'high quality product',
+
+        },
+        {
+            _id:'5',
+            name:'Name Slim Shirt',
+            category: 'Shirts',
+            image:'/images/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description:'high quality product',
+
+        },
+        {
+            _id:'6',
+            name:'Name Slim Shirt',
+            category: 'Shirts',
+            image:'/images/p1.jpg',
+            price: 1201,
+            brand: 'Nike',
+            rating: 4.2,
+            numReviews: 10,
+            description:'high quality product',
+
+        },
+        {
+            _id:'7',
+            name:'Deol Slim Shirt',
+            category: 'Shirts',
+            image:'/images/p1.jpg',
+            price: 120,
+            brand: 'Deol',
+            rating: 2.5,
+            numReviews: 10,
+            description:'high quality product',
+
+        },
+    ],
+};
+export default data;
